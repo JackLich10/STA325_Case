db42979ae3b2bfd27b249aaa423a93380a0edc4c
XLURTANG <lyt3@duke.edu>
2021-10-07 21:48:59 -0400

    added qqplot

--- R/explore_linda.R
+++ R/explore_linda.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(boot)
+library(MASS)
 set.seed(1)
 
 train <- readr::read_csv("data/data-train.csv")
@@ -7,10 +8,11 @@ train <- train %>%
   # change Fr to numeric
   mutate(Fr = as.numeric(Fr)) %>%
   # transforming and scaling the response variables
-  mutate(R_moment_1 = (R_moment_1 - min(R_moment_1))/(max(R_moment_1) - min(R_moment_1)),
-         R_moment_2 = (R_moment_2 - min(R_moment_2))/(max(R_moment_2) - min(R_moment_2)),
-         R_moment_3 = (R_moment_3 - min(R_moment_3))/(max(R_moment_3) - min(R_moment_3)),
-         R_moment_4 = (R_moment_4 - min(R_moment_4))/(max(R_moment_4) - min(R_moment_4))) %>%
+  mutate(across(c(R_moment_1, R_moment_2, R_moment_3, R_moment_4), scale)) %>%
+  # mutate(R_moment_1 = (R_moment_1 - min(R_moment_1))/(max(R_moment_1) - min(R_moment_1)),
+  #        R_moment_2 = (R_moment_2 - min(R_moment_2))/(max(R_moment_2) - min(R_moment_2)),
+  #        R_moment_3 = (R_moment_3 - min(R_moment_3))/(max(R_moment_3) - min(R_moment_3)),
+  #        R_moment_4 = (R_moment_4 - min(R_moment_4))/(max(R_moment_4) - min(R_moment_4))) %>%
   # making Fr and Re categorical
   mutate(across(c(Fr, Re), factor)) %>%
   # taking the square root of St
@@ -34,12 +36,14 @@ x <- summary(lm(R_moment_1 ~ St + Fr + Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_1 ~ St + Fr + Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 x <- summary(lm(R_moment_1 ~ St + Fr*Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_1 ~ St + Fr*Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 data_1 <- train_long %>%
@@ -48,6 +52,7 @@ x <- summary(lm(moment_value ~ poly(St, 3)*interaction, data = data_1))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(moment_value ~ poly(St, 3)*interaction, data = data_1)
+qqnorm(model$residuals)
 cv.glm(data_1, model, K = 5)$delta[1]
 
 
@@ -57,12 +62,14 @@ x <- summary(lm(R_moment_2 ~ St + Fr + Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_2 ~ St + Fr + Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 x <- summary(lm(R_moment_2 ~ St + Fr*Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_2 ~ St + Fr*Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 data_2 <- train_long %>%
@@ -71,6 +78,7 @@ x <- summary(lm(moment_value ~ poly(St, 3)*interaction, data = data_2))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(moment_value ~ poly(St, 3)*interaction, data = data_2)
+qqnorm(model$residuals)
 cv.glm(data_2, model, K = 5)$delta[1]
 
 
@@ -80,12 +88,14 @@ x <- summary(lm(R_moment_3 ~ St + Fr + Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_3 ~ St + Fr + Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 x <- summary(lm(R_moment_3 ~ St + Fr*Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_3 ~ St + Fr*Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 data_3 <- train_long %>%
@@ -94,6 +104,7 @@ x <- summary(lm(moment_value ~ poly(St, 3)*interaction, data = data_3))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(moment_value ~ poly(St, 3)*interaction, data = data_3)
+qqnorm(model$residuals)
 cv.glm(data_3, model, K = 5)$delta[1]
 
 
@@ -103,12 +114,14 @@ x <- summary(lm(R_moment_4 ~ St + Fr + Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_4 ~ St + Fr + Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 x <- summary(lm(R_moment_4 ~ St + Fr*Re, data = train))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(R_moment_4 ~ St + Fr*Re, data = train)
+qqnorm(model$residuals)
 cv.glm(train, model, K = 5)$delta[1]
 
 data_4 <- train_long %>%
@@ -117,4 +130,5 @@ x <- summary(lm(moment_value ~ poly(St, 2)*interaction, data = data_4))
 print("adj R^2")
 x$adj.r.squared
 model <- glm(moment_value ~ poly(St, 2)*interaction, data = data_4)
+qqnorm(model$residuals)
 cv.glm(data_4, model, K = 5)$delta[1]
