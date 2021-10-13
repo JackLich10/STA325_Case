4a253367232d2bf953bc97f8aa368acf78b33888
jack lichtenstein <jack.lichtenstein@duke.edu>
2021-10-12 20:05:56 -0400

    refactor cross validation to add in sqrt, log arguments

--- R/crossfold_validation.R
+++ R/crossfold_validation.R
@@ -21,43 +21,74 @@ set.seed(234)
 folds <- rsample::vfold_cv(tr, v = 5)
 
 # function to train on a given fold, using given degree, predicting given moment
-lm_cv <- function(fold, degree, moment) {
+lm_cv <- function(fold, degree, moment, sqrt = TRUE, log = TRUE) {
   data <- rsample::analysis(folds$splits[[fold]]) %>%
     dplyr::mutate(target = !!dplyr::sym(paste0("R_moment_", moment)))
   assess <- rsample::assessment(folds$splits[[fold]]) %>%
     dplyr::mutate(target = !!dplyr::sym(paste0("R_moment_", moment)))
-  
+
+  if (isTRUE(sqrt)) {
+    data <- data %>%
+      dplyr::mutate(St = sqrt(St))
+    assess <- assess %>%
+      dplyr::mutate(St = sqrt(St))
+  }
+
+  if (isTRUE(log)) {
+    data <- data %>%
+      dplyr::mutate(target = log(target))
+    assess <- assess
+  }
+
   mod <- data %>%
-    lm(target ~ poly(sqrt(St), degree)*interaction, data = .)
-  
+    lm(target ~ poly(St, degree)*interaction, data = .)
+
   train_adj_r2 <- broom::glance(mod)$adj.r.squared
-  
-  assess %>%
+
+  assess <- assess %>%
     dplyr::mutate(pred = predict(mod, ., type = "response") %>% as.numeric())
+
+  if (isTRUE(log)) {
+    assess <- assess %>%
+      dplyr::mutate(pred = exp(pred))
+  }
+  return(assess)
 }
 
+assess %>%
+  ggplot(aes(pred, target)) +
+  geom_point() +
+  geom_abline()
 
 # compute CV
 cv <- tidyr::crossing(fold = 1:5,
                       degree = 1:3,
-                      moment = 1:4) %>%
-  dplyr::mutate(cv = purrr::pmap(list(fold, degree, moment),
+                      moment = 1:4,
+                      sqrt = c(TRUE, FALSE),
+                      log = c(TRUE, FALSE)) %>%
+  dplyr::mutate(cv = purrr::pmap(list(fold, degree, moment, sqrt, log),
                                  ~ lm_cv(fold = ..1,
                                          degree = ..2,
-                                         moment = ..3))) %>%
+                                         moment = ..3,
+                                         sqrt = ..4,
+                                         log = ..5))) %>%
   tidyr::unnest(cv)
 
 # Plot
 cv %>%
-  dplyr::group_by(moment = factor(moment), degree) %>%
+  filter(sqrt == TRUE) %>%
+  # filter(moment == 3) %>%
+  # filter(degree < 3) %>%
+  dplyr::group_by(moment = factor(moment), degree, sqrt, log) %>%
   dplyr::summarise(rmse = sqrt(mean((pred-target)^2)),
                    mae = mean(abs(pred-target)),
                    broom::glance(lm(target ~ pred, data = dplyr::cur_data())),
-                   .groups = "drop") %>% view(title = "new")
+                   .groups = "drop") %>%
+# %>% view(title = "new")
   dplyr::mutate(moment = paste0("Moment: ", moment)) %>%
   tidyr::pivot_longer(cols = c(rmse, mae, adj.r.squared)) %>%
   tidyr::unite(type, moment, name, sep = ": ", remove = FALSE) %>%
-  ggplot(aes(degree, value, color = moment)) +
+  ggplot(aes(degree, value, color = moment, lty = log)) +
   geom_line() +
   geom_point() +
   guides(color = "none") +
@@ -66,3 +97,8 @@ cv %>%
 #### degree of polynomial 2 probably makes the most sense!
 ### Note that we still need to sqrt() and then do poly(, degree = 2)
 ### since poly() is different than just ^2
+
+train_long %>%
+  ggplot(aes(log(moment_value))) +
+  geom_histogram() +
+  facet_wrap(~ moment_type, scales = "free_x")
